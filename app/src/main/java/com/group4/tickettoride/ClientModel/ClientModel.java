92e1b332bd082ffc4865823300815f22d0534b25
Tyler Bowers <tyler.j.bowers@gmail.com>
2017-05-15 12:21:53 -0600

    playerList removed from ClientModel

--- app/src/main/java/com/group4/tickettoride/ClientModel/ClientModel.java
+++ app/src/main/java/com/group4/tickettoride/ClientModel/ClientModel.java
@@ -1,7 +1,6 @@
 package com.group4.tickettoride.ClientModel;
 
 import com.group4.shared.Model.GameList;
-import com.group4.shared.Model.PlayerList;
 import com.group4.shared.Model.User;
 
 import java.util.Observable;
@@ -14,13 +13,11 @@ public class ClientModel extends Observable {
 
     private User user;
     private GameList gameList;
-    private PlayerList playerList;
     // Todo: int commandIDIndex, last command run
 
-    public ClientModel(User user, GameList gameList, PlayerList playerList) {
+    public ClientModel(User user, GameList gameList) {
         this.user = user;
         this.gameList = gameList;
-        this.playerList = playerList;
     }
 
     public User getUser() {
@@ -38,14 +35,6 @@ public class ClientModel extends Observable {
     public void setGameList(GameList gameList) {
         this.gameList = gameList;
         notifyObservers(gameList);
-    }
-
-    public PlayerList getPlayerList() {
-        return playerList;
-    }
-
-    public void setPlayerList(PlayerList playerList) {
-        this.playerList = playerList;
     }
 
     /**
