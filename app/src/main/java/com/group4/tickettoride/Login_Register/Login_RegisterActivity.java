2e06d2b087ba5fad89ec1c9db2ffc98734423933
abgill <gill.andrew.95@gmail.com>
2017-05-18 12:30:01 -0600

    I set the login activity to go to immersive mode

--- app/src/main/java/com/group4/tickettoride/Login_Register/Login_RegisterActivity.java
+++ app/src/main/java/com/group4/tickettoride/Login_Register/Login_RegisterActivity.java
@@ -18,12 +18,30 @@ public class Login_RegisterActivity extends AppCompatActivity implements ILogin_
     Button login;
     Button register;
     Login_RegisterPresenter presenter;
+    View decorView;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
+
+        decorView = getWindow().getDecorView();
+
+
 
         this.presenter = new Login_RegisterPresenter(this);
 
