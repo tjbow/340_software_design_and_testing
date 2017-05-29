d562b3d330672d7fd0b9e89bcbea89edb30ee686
tscheney <thomascheneyyzs@gmail.com>
2017-05-28 19:12:38 -0600

    Destination picker is done.  I added a textbox at the top that we may or may not keep.  For now it is there to indicate the correct cities.

--- app/src/main/java/com/group4/tickettoride/Game/GameFragments/DestCardPickerPresenter.java
+++ app/src/main/java/com/group4/tickettoride/Game/GameFragments/DestCardPickerPresenter.java
@@ -2,11 +2,17 @@ package com.group4.tickettoride.Game.GameFragments;
 
 import com.group4.shared.Model.Deck.DestinationCard;
 import com.group4.shared.Model.Deck.DestinationCardDeck;
+import com.group4.shared.Model.Player;
+import com.group4.shared.Model.Results;
 import com.group4.tickettoride.ClientModel.ClientModel;
+import com.group4.tickettoride.Network.ServerProxy;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+
+import javax.sql.StatementEvent;
 
 /**
  * Created by Tom on 5/26/2017.
@@ -15,38 +21,86 @@ import java.util.Observer;
 public class DestCardPickerPresenter implements Observer, IDestCardPickerPresenter
 {
     private IDestCardPickerFragment destFragment;
-    private DestinationCardDeck cards;
+    Player player;
 
-    public DestCardPickerPresenter(IDestCardPickerFragment fragment)
+    private List<DestinationCard> cards;
+
+    public DestCardPickerPresenter(DestCardPickerFragment fragment)
     {
         destFragment = fragment;
         ClientModel.SINGLETON.addObserver(this);
-        //cards = ClientModel.SINGLETON.
+        player = ClientModel.SINGLETON.getPlayer();
+        if(fragment.getMinNumSelected() == 2) // beginning of game
+        {
+            cards = ClientModel.SINGLETON.getPlayer().getPlayerHand().getDestinationCards().getDestDeck();
+        }
+        else
+        {
+            cards = drawThreeNewCards();
+        }
+
     }
 
     @Override
-    public void addDestination(List<String> cardIDs)
+    public void addDestination(List<DestinationCard> cards)
     {
-
+        for(DestinationCard card : cards)
+        {
+            player.getPlayerHand().getDestinationCards().add(card);
+        }
     }
 
-
+    @Override
+    public void returnSelectedDestCards(List<Integer> selCards)
+    {
+        List<DestinationCard> returnCards = new ArrayList<>();
+        for(Integer sel : selCards)
+        {
+            returnCards.add(cards.get(sel));
+        }
+        ServerProxy.SINGLETON.returnDestinationCard(returnCards);
+    }
 
     @Override
     public List<DestinationCard> drawThreeNewCards()
     {
-        return null;
-    }
+        List<Integer> destIDBeforeDraw = new ArrayList<>(); // grab list before draw
+        for(DestinationCard card : player.getPlayerHand().getDestinationCards().getDestDeck())
+        {
+            destIDBeforeDraw.add(card.getId());
+        }
+        // draw cards into player hand
+        Results results = ServerProxy.SINGLETON.drawDestinationCards(player.getUserName());
 
-    @Override
-    public void returnDestinationCards(DestinationCardDeck cards)
-    {
-
+        // if success, get the newly drawn cards
+        if(results.isSuccess())
+        {
+            List<DestinationCard> destCardsAfterDraw = player.getPlayerHand().getDestinationCards().getDestDeck();
+            List<DestinationCard> newDestCards = new ArrayList<>();
+            for(DestinationCard card : destCardsAfterDraw)
+            {
+                if(!destIDBeforeDraw.contains(card.getId())) // this card wasn't present before drawn, it is new
+                {
+                    newDestCards.add(card);
+                }
+            }
+            return newDestCards;
+        }
+        else
+        {
+            System.out.println("Failed to draw destination cards");
+            throw new RuntimeException(results.getErrorInfo());
+        }
     }
 
     @Override
     public void update(Observable observable, Object o)
     {
 
+    }
+
+    public List<DestinationCard> getCards()
+    {
+        return cards;
     }
 }
